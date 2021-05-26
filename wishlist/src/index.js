e8861bf4719a339e2cdaf8e9fbfdf6e46c93a066
LameckVieira <lameck.v.barbosa@gmail.com>
2021-05-26 16:32:00 -0300

    react js criado

--- wishlist/src/index.js
+++ wishlist/src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+
+import App from './pages/home/App';
+import tiposDesejos from './pages/tiposDesejos/tiposDesejos';
+import NotFound from'./pages/notFound/notFound';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App}/> {/*HomePage*/}
+        <Route path="/tiposDesejos" component={tiposDesejos}/> {/*tiposDesejos page*/}
+        <Route exact path="/notfound" component={NotFound} /> {/*Not Found page*/}
+        <Redirect to="/notfound" /> {/*Redirects to NotFound if no route is found*/}
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ wishlist/src/pages/notFound/notFound.js
@@ -0,0 +1,11 @@
+function NotFound() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> 404 - Not Found</h1>
+        </header>
+      </div>
+    );
+  }
+
+export default NotFound;
